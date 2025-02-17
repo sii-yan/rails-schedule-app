e6c1d6b870f26e16fc90c04a83eba324b2f99c78
sii-yan <naobeedesign@gmail.com>
2025-02-17 17:33:04 +0900

    add/ルーティングの設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :schedules
+  get 'schedules/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :schedules
 end
